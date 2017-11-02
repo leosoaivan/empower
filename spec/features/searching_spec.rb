e6f3a94dc11a43b7d119a351d87167bb5da50df6
L. Soai-Van <leo.soaivan@gmail.com>
2017-11-01 22:14:09 -0400

    Add test for client search by/with ID

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Search management' do
+feature 'Client search management' do
   let (:user) { FactoryGirl.create(:user) }
   let! (:client1) { 
     FactoryGirl.create(:client,
@@ -22,6 +22,8 @@ feature 'Search management' do
       firstname: "Janice",
       lastname: "Hopper") 
   }
+
+  let (:data_row) { 'tbody tr' }
   
   before :each do
     log_in(user)
@@ -38,71 +40,158 @@ feature 'Search management' do
     end
 
     it "returns all clients" do
-      expect(find('table')).to have_css('td.firstname', count: Client.count)
+      expect(find('table')).to have_css(data_row, count: Client.count)
     end
   end
 
-  describe "searching for clients" do
-    context "with full firstname" do
-      it "edits the table with matching results" do
-        fill_in 'firstname', with: client1.firstname
-        fill_in 'lastname', with: nil
+  describe "searching by ID" do
+    context "with a valid ID" do
+      before :each do
+        fill_in 'id', with: client1.id
         click_button 'Search'
-        expect(page).to have_css('td.firstname', count: 2)
+      end
+      
+      it "edits the table with matching result" do
+        expect(find(data_row)).to have_content(client1.id)
+      end
+
+      it "returns a specific client" do
+        expect(page).to have_css(data_row, count: 1)
       end
     end
 
-    context "with first few letters of firstname" do
-      it "edits the table with matching results" do
-        fill_in 'firstname', with: client1.firstname[0, 3]
-        fill_in 'lastname', with: nil
-        click_button 'Search'
-        expect(page).to have_css('td.firstname', count: 2)
-      end
-    end
-
-    context "with full lastname" do
-      it "edits the table with matching results" do
-        fill_in 'firstname', with: nil
-        fill_in 'lastname', with: client1.lastname
-        click_button 'Search'
-        expect(page).to have_css('td.lastname', count: 2)
-      end
-    end
-
-    context "with first few letters of lastname" do
-      it "edits the table with matching results" do
-        fill_in 'firstname', with: nil
-        fill_in 'lastname', with: client1.lastname[0, 3]
-        click_button 'Search'
-        expect(page).to have_css('td.lastname', count: 2)
-      end
-    end
-
-    context "with firstname AND lastname" do
-      it "edits the table with matching results" do
+    context "with a valid ID and valid full name" do
+      before :each do
+        fill_in 'id', with: client1.id
         fill_in 'firstname', with: client1.firstname
         fill_in 'lastname', with: client1.lastname
         click_button 'Search'
-        expect(page).to have_css('td.firstname', count: 1)
+      end
+
+      it "edits the table with matching result" do
+        expect(find(data_row)).to have_content(client1.id)
+      end
+
+      it "returns a specific client" do
+        expect(page).to have_css(data_row, count: 1)
       end
     end
 
-    context "with first few letters of both firstname AND lastname" do
+    context "with a valid ID, valid firstname, and invalid lastname" do
+      it "returns no hits" do
+        fill_in 'id', with: client1.id
+        fill_in 'firstname', with: client1.firstname
+        fill_in 'lastname', with: "Grace"
+        click_button 'Search'
+        expect(page).to_not have_css(data_row)
+      end
+    end
+
+    context "with a valid ID, invalid firstname, and valid lastname" do
+      it "returns no hits" do
+        fill_in 'id', with: client1.id
+        fill_in 'firstname', with: "Topher"
+        fill_in 'lastname', with: "Smith"
+        click_button 'Search'
+        expect(page).to_not have_css(data_row)
+      end
+    end
+
+    context "with an invalid ID" do      
+      it "returns no hits" do
+        fill_in 'id', with: 100
+        click_button 'Search'
+        expect(page).to_not have_css(data_row)
+      end
+    end
+
+    context "with an invalid ID, valid firstname, and invalid lastname" do
+      it "returns no hits" do
+        fill_in 'id', with: 100
+        fill_in 'firstname', with: client1.firstname
+        fill_in 'lastname', with: "Grace"
+        click_button 'Search'
+        expect(page).to_not have_css(data_row)
+      end
+    end
+
+    context "with an invalid ID, invalid firstname, and valid lastname" do
+      it "returns no hits" do
+        fill_in 'id', with: 100
+        fill_in 'firstname', with: "Topher"
+        fill_in 'lastname', with: client1.lastname
+        click_button 'Search'
+        expect(page).to_not have_css(data_row)
+      end
+    end
+  end
+  
+  describe "searching by name only" do
+    context "with a valid firstname" do
+      it "edits the table with matching results" do
+        fill_in 'firstname', with: client1.firstname
+        click_button 'Search'
+        expect(page).to have_css(data_row, count: 2)
+      end
+    end
+
+    context "with a partial firstname" do
+      it "edits the table with matching results" do
+        fill_in 'firstname', with: client1.firstname[0, 3]
+        click_button 'Search'
+        expect(page).to have_css(data_row, count: 2)
+      end
+    end
+
+    context "with a valid lastname" do
+      it "edits the table with matching results" do
+        fill_in 'lastname', with: client1.lastname
+        click_button 'Search'
+        expect(page).to have_css(data_row, count: 2)
+      end
+    end
+
+    context "with a partial lastname" do
+      it "edits the table with matching results" do
+        fill_in 'lastname', with: client1.lastname[0, 3]
+        click_button 'Search'
+        expect(page).to have_css(data_row, count: 2)
+      end
+    end
+
+    context "with valid firstname AND lastname" do
+      it "edits the table with matching results" do
+        fill_in 'firstname', with: client1.firstname
+        fill_in 'lastname', with: client1.lastname
+        click_button 'Search'
+        expect(page).to have_css(data_row, count: 1)
+      end
+    end
+
+    context "with partial firstname AND lastname" do
       it "edits the table with matching results" do
         fill_in 'firstname', with: client1.firstname[0, 3]
         fill_in 'lastname', with: client1.lastname[0, 3]
         click_button 'Search'
-        expect(page).to have_css('td.firstname', count: 1)
+        expect(page).to have_css(data_row, count: 1)
       end
     end
 
-    context "with an incorrect query" do
-      it "returns a blank table" do
+    context "with an invalid first name" do
+      it "returns no hits" do
         fill_in 'firstname', with: client1.firstname
         fill_in 'lastname', with: "Mike"
         click_button 'Search'
-        expect(page).to_not have_css('td.firstname')
+        expect(page).to_not have_css(data_row)
+      end
+    end
+
+    context "with an invalid last name" do
+      it "returns no hits" do
+        fill_in 'firstname', with: "Topher"
+        fill_in 'lastname', with: client1.lastname
+        click_button 'Search'
+        expect(page).to_not have_css(data_row)
       end
     end
   end
