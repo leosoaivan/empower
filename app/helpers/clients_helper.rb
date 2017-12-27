237266819853d5a452624fee2a619240ec59dce1
Leonard Soai-Van <leo.soaivan@gmail.com>
2017-12-27 01:30:30 -0500

    Add moving form-field labels

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -2,4 +2,8 @@ module ClientsHelper
   def form_submit_action
     @client.persisted? ? "Edit Client" : "Create Client"
   end
+
+  def set_label_class(attr)
+    @client.send(attr).present? ? "active" : ""
+  end
 end
