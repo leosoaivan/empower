018a005d7762ee8e1bede411ddefaa83dd0346b5
Leonard Soai-Van <leo.soaivan@gmail.com>
2017-12-28 23:08:17 -0500

    Edit factories to match model validations

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,19 +1,12 @@
 FactoryGirl.define do
-  factory :client do
+  factory :client, aliases: [:petitioner, :respondent] do
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
-    dob { Faker::Date.birthday(18, 65) }
-    telephone { Faker::PhoneNumber.cell_phone }
-    address { 
-      { street: Faker::Address.street_address,
-        secondary: Faker::Address.secondary_address,
-        city: Faker::Address.city,
-        state: Faker::Address.state_abbr,
-        zipcode: Faker::Address.zip 
-      }
-    }
-    children { rand(0..10) }
-    gender "Female"
-    email { Faker::Internet.email}
+  end
+
+  factory :client_with_episodes, parent: :client do
+    after(:create) do |client, evaluator|
+      create_list(:episode, 1, petitioner: client)
+    end
   end
 end
--- spec/factories/episodes.rb
+++ spec/factories/episodes.rb
@@ -1,10 +1,7 @@
 FactoryGirl.define do
   factory :episode do
     petitioner_id ""
-    respondent_id ""
     relationship ['dating/romantic/sexual','shared residence']
     victimization ['intimate partner violence']
-    arrest false
-    open true
   end
 end
