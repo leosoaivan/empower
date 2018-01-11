fe4e8cca78936f3d0ebb2da47e347ca39ba9cbc5
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-01-10 23:09:30 -0500

    Remove Bullet from test environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,19 +44,6 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-
-  # Bullet
-  if Bullet.enable?
-    config.before(:each) do
-      Bullet.start_request
-    end
-
-    config.after(:each) do
-      Bullet.perform_out_of_channel_notifications if Bullet.notification?
-      Bullet.end_request
-    end
-  end
-
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
