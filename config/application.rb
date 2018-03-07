b5de205e2785d7707f686d223914abaec08e3556
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-03-06 22:00:36 -0500

    Minor setup

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Empower
         routing_specs: false,
         controller_specs: false,
         request_specs: false
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
 end
